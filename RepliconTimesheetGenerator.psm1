4ed828404bcb37a67a4e3c848485ac4c3abac23e
Dodzidenu Y. Dzakuma <ddzakuma@kmsnet.com>
2016-12-30 08:09:21 -0600

    I have the overall structure of the project down.

--- RepliconTimesheetGenerator.psm1
+++ RepliconTimesheetGenerator.psm1
@@ -14,13 +14,13 @@ Resolve-Path |
 Where-Object { -not ($_.ProviderPath.ToLower().Contains(".tests.")) } |
 ForEach-Object { . $_.ProviderPath }
 
-Export-ModuleMember -Function "New-TimesheetSummary"
-Export-ModuleMember -Function "Read-TimesheetData"
-Export-ModuleMember -Function "Format-TimesheetData"
-Export-ModuleMember -Function "Select-DaysWorked"
-Export-ModuleMember -Function "Set-PlantsAffected"
-Export-ModuleMember -Function "Group-TimesheetData"
-Export-ModuleMember -Function "Group-DailyTimeSegments"
-Export-ModuleMember -Function "Measure-TimeWorked"
-Export-ModuleMember -Function "Measure-PlantTimeAllotment"
-Export-ModuleMember -Function "Write-TimesheetSummary"
+Export-ModuleMember -Function New-TimesheetSummary
+Export-ModuleMember -Function Read-TimesheetData
+Export-ModuleMember -Function Format-TimesheetData
+Export-ModuleMember -Function Select-DaysWorked
+Export-ModuleMember -Function Set-PlantsAffected
+Export-ModuleMember -Function Group-TimesheetData
+Export-ModuleMember -Function Group-DailyTimeSegments
+Export-ModuleMember -Function Measure-TimeWorked
+Export-ModuleMember -Function Measure-PlantTimeAllotment
+Export-ModuleMember -Function Write-TimesheetSummary
--- functions/New-TimesheetSummary.ps1
+++ functions/New-TimesheetSummary.ps1
@@ -1,5 +1,20 @@
 function New-TimesheetSummary
 {
+	[CmdletBinding(SupportsShouldProcess=$true)]
+	param (
+		[Parameter(Mandatory=$true)]
+		[ValidateNotNullOrEmpty()]
+		[ValidateScript({Test-Path $_ -PathType Leaf})]
+		[string]$FilePath,
+
+		[Parameter(Mandatory=$true)]
+		[ValidateNotNullOrEmpty()]
+		[ValidateScript({(Test-Path $_ -IsValid) -and !(Test-Path $_ -PathType Container)})]
+		[string]$Destination
+	)
+
+	Write-Verbose -Message ("Opening [$FilePath] to load raw report data.")
+
 	Read-TimesheetData
 	Format-TimesheetData
 	Select-DaysWorked
@@ -9,4 +24,7 @@ function New-TimesheetSummary
 	Measure-TimeWorked
 	Measure-PlantTimeAllotment
 	Write-TimesheetSummary	
+
+	Write-Verbose -Message ("Writing timesheet summary to [$Destination].")
 }
+#New-TimesheetSummary -FilePath .\Desktop\ManicTimeSearch_2016-12-05.csv -Destination .\Desktop\sample-timesheet.txt -Verbose
